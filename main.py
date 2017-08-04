a879e7fe870e2a4da57c9deee6fa3b6bfab959fa
Ulises Avila Camargo <ulisesavila04@gmail.com>
2017-08-04 12:45:06 -0500

    Finished get closer of original duration

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ creator = listOfTracks[1]
 title = listOfTracks[2]
 length = listOfTracks[3]
 idOfVideo = listOfTracks[4]
+listOfTracks[4] = listOfTracks[4].astype(str)
 
 for index in range(1000):
     query =  "{} {}".format(creator[index], title[index])
     print(query)
     possibleResults = search.searchVideos(query)
     lengths = [0, 0, 0]
-
-    if len(possibleResults) != 0:
+    if len(possibleResults) > 0:
         lengths[0] = search.getLengthOfVideo(possibleResults[0])
         lengths[1] = search.getLengthOfVideo(possibleResults[1])
         lengths[2] = search.getLengthOfVideo(possibleResults[2])
@@ -33,14 +33,18 @@ for index in range(1000):
         if len(isInRange) == 1:
             key = list(isInRange[0])
             print(key[0])
-            listOfTracks.set_value(4, index, possibleResults[key[0]])
+            print(possibleResults[key[0]])
+            listOfTracks.set_value(index, 4, possibleResults[key[0]])
         elif len(isInRange) > 1:
             for x in isInRange:
                 x.pop("inRange", None)
             newList = []
             for x in isInRange:
                 newList.append(min(x.items()))
-            lesserTuple = min(newList, key=lambda x: x[1])
+            #min(myList, key=lambda x:abs(x-myNumber))
+            lesserTuple = min(newList, key=lambda x: abs(x[1]-length[index]))
             print(lesserTuple[0])
-            listOfTracks.set_value(4, index, possibleResults[lesserTuple[0]])
+            print(possibleResults[lesserTuple[0]])
+            listOfTracks.set_value(index, 4, possibleResults[lesserTuple[0]])
+
 listOfTracks.to_csv("final.csv")
