e49d6a0b5907c8bfdfb93c1de064975219df106a
Ulises Avila Camargo <ulisesavila04@gmail.com>
2017-08-04 02:02:59 -0500

    Search list of videos finished

---
+++ search.py
@@ -0,0 +1,36 @@
+#Module for searching videos from the youtube api
+import pandas
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+
+#
+DEVELOPER_KEY = "AIzaSyBZhiI5trjy4ZFysrYD54_E9sDMR3aKfpo"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+def searchVideos(query="fate grand order"):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+    response = youtube.search().list(
+        q = query,
+        part = "id",
+        maxResults = 3,
+        type = ["video"]
+    ).execute()
+
+    listOfIds = []
+    for video in response["items"]:
+        listOfIds.append(video["id"]["videoId"])
+
+    return listOfIds
+
+def getLengthOfVideos(videos):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+    response = youtube.videos().list(
+        part = "id,contentDetails",
+        id = videos[0]
+    ).execute()
+
+    return response
+
+print(searchVideos())
